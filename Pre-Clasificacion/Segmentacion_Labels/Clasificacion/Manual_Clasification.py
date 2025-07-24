9422c0b1412bc44cce0cd0e705775418a0d740e3
ShryhakAkizuki <oriverosp@unal.edu.co>
2025-07-23 21:48:59 -0500

    .

--- Pre-Clasificacion/Segmentacion_Labels/Clasificacion/Manual_Clasification.py
+++ Pre-Clasificacion/Segmentacion_Labels/Clasificacion/Manual_Clasification.py
@@ -13,7 +13,7 @@ import cv2
 # ------------ Funciones ------------
 def Manual_Clasification(path: str) -> int:
     """
-    Abre una ventana que contiene la imagen a analizar. Si se presiona "y", se clasifica como 1 y si no como 0.
+    Abre una ventana que contiene la imagen a analizar. Si se presiona "y", se clasifica como 1 y si no como 0.
 
     Args:
         path (str): Ruta relativa o absoluta hacia el archivo de imagen a analizar.
@@ -21,16 +21,18 @@ def Manual_Clasification(path: str) -> int:
         int: Retorna la etiqueta como un entero, si es 1 significa (presencia de embarcación) y si es 0 como (ausencia de embarcación).
     """
     
-    # Imprime la ruta en la cual se esta analizando la imagen para tener una referencia en consola
-    print(path)
+    print(path)                                         # Imprime la ruta de la imagen que se esta analizando
+    cv2.imshow("Detecciones Radar", cv2.imread(path))   # Muestra la imagen en una ventana emergente
+    Tecla = cv2.waitKey(0)                              # Espera indefinidamente hasta presionar una tecla
 
-    # Mostrar la imagen con OpenCV
-    cv2.imshow("Detecciones Radar", cv2.imread(path))
+    if(Tecla == ord('y')):                              # Si la tecla es "y", devolver 1 (Etiqueta para presencia de embarcaciones)
+        print("✅")  
+        return 1 
+    else:                                               # Si se presiona otra tecla devolver 0 (Etiqueta para ausencia de embarcaciones)
+        print("❌")  
+        return 0                  
 
-    Tecla = cv2.waitKey(0)  # Espera indefinidamente hasta presionar una tecla
-
-    if(Tecla == ord('y')): return 1 # Si la tecla es y, devolver 1 (Etiqueta para presencia de embarcaciones)
-    else: return 0                  # Si se presiona otra tecla devolver 0 (Etiqueta para ausencia de embarcaciones)
+# -----------------------------------
 
 if __name__ == "__main__":
 
@@ -43,21 +45,19 @@ if __name__ == "__main__":
 
         
         reader = csv.DictReader(infile)         # Lee el Header Original
-        Nuevas_Columnas = ["Main_Label"         # Nuevo Header
-        ]
-
+        Nuevas_Columnas = ["Main_Label"]        # Nuevo Header
+        
         writer = csv.DictWriter(outfile, fieldnames=(reader.fieldnames+Nuevas_Columnas))    # Escribe el nuevo Header
         writer.writeheader()
 
         for row in reader:                     # Por cada elemento del archivo CSV
             if row["Image_Name"]!="N/A":       # Si encuentra una imagen
                 Ruta_Image=f"{row["Path"]}\\{row["Image_Path"]}\\{row["Image_Name"]}"   # Obtiene su ruta
-
-                results = Manual_Clasification(Ruta_Image) # La clasifica
-
+                results = Manual_Clasification(Ruta_Image)                              # La clasifica
             else:
                 results = -1                # Si no la encuentra, obtiene valores por defecto
 
             row["Main_Label"] = results     # Escribe los valores respectivos al CSV
             writer.writerow(row)
+            
     # -------------------------------------------------------------------------------------------------
